9c459ab2609caf4ae93012b636f839dec0d0f629
P2FU2 <pedromilani14@gmail.com>
2025-11-02 02:47:38 -0300

    Fix Netlify publish directory 5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // nada de output:'export' aqui
+  // nada de output: 'export' aqui
+  images: {
+    // se usar next/image com domínios externos, declare aqui
+    // domains: ['images.ctfassets.net', '...']
+  },
 }
 module.exports = nextConfig
